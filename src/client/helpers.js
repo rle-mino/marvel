9ead9366ddf94583aceb279d701f5d7f86fe50e4
rle-mino <raphael@solive.biz>
2017-08-25 15:40:52 +0200

    fix helper

--- src/client/helpers.js
+++ src/client/helpers.js
@@ -1,3 +1,6 @@
+import crypto from 'crypto';
+import { API_PUBLIC, API_PRIVATE } from './constants';
+
 const generateAuthParams = () => {
   const timestamp = Math.round(+new Date() / 1000);
   const hash = crypto.createHash('md5').update(`${timestamp}${API_PRIVATE}${API_PUBLIC}`).digest('hex');
